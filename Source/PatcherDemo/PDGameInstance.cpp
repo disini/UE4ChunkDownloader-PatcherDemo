992c49e0ffeab76559ac9d4001c327e9069c3a6f
GiGi <disini@live.cn>
2021-11-08 17:19:55 +0800

    implement the 'InitPatching' and 'OnPatchVersionResponse' functions

--- Source/PatcherDemo/PDGameInstance.cpp
+++ Source/PatcherDemo/PDGameInstance.cpp
@@ -6,6 +6,9 @@
 #include "Misc/CoreDelegates.h"
 #include "AssetRegistryModule.h"
 
+
+DEFINE_LOG_CATEGORY_STATIC(LogPatch, Display, Display);
+
 void UPDGameInstance::Init()
 {
 	Super::Init();
@@ -21,11 +24,81 @@ void UPDGameInstance::Shutdown()
 
 void UPDGameInstance::InitPatching(const FString& VariantName)
 {
+	// save the patch platform
+	PatchPlatform = VariantName;
+	
+	// get the HTTP module
+	FHttpModule& Http = FHttpModule::Get();
+
+	// create a new HTTP request and bind the response callback
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http.CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UPDGameInstance::OnPatchVersionResponse);
+
+	// configure and send the request
+	Request->SetURL(PatchVersionURL);
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+
 
 }
 
 void UPDGameInstance::OnPatchVersionResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
+	// deployment name to use for this patcher configuration
+	const FString DeploymentName = "Patcher-Live";
+
+	// content build ID. Our Http response will provide this info
+	// so we can update to new patch versions as they become available on the server
+	FString ContentBuildId = Response->GetContentAsString();
+
+	UE_LOG(LogPatch, Display, TEXT("Patch Content ID Response: %s"), *ContentBuildId);
+
+	// -----------------------------------------------------
+
+	UE_LOG(LogPatch, Display, TEXT("Getting Chunk Downloader"));
+
+	// initialize the chunk downloader with the chosen platform
+	TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetOrCreate();
+	Downloader->Initialize(PatchPlatform, 8);
+
+	UE_LOG(LogPatch, Display, TEXT("Loading Cached Build ID"));
+
+	// Try to load the cached build manifest data if it exists.
+	// This will populate the Chunk Downloader state with the most recently downloaded manifest data.
+	if (Downloader->LoadCachedBuild(DeploymentName))
+	{
+		UE_LOG(LogPatch, Display, TEXT("Cached Build Succeeded"));
+	}
+	else {
+		UE_LOG(LogPatch, Display, TEXT("Cached Build Failed"));
+	}
+
+	UE_LOG(LogPatch, Display, TEXT("Updating Build Manifest"));
+
+	// Check with the server if there's a newer manifest file that matches our data, and download it.
+	// ChunkDownloader uses a lambda function to let us know when the async download is complete.
+	TFunction<void(bool)> ManifestCompleteCallback = [this](bool bSuccess)
+	{
+		// write to the log
+		if (bSuccess)
+		{
+			UE_LOG(LogPatch, Display, TEXT("Manifest Update Succeeded"));			
+		}
+		else {
+			UE_LOG(LogPatch, Display, TEXT("Manifest Update Failed"));
+		}
+
+		// Flag the manifest's status so we know whether it's safe to patch or not
+		bIsDownloadManifestUpToDate = bSuccess;
+
+		// call our delegate to let the game know we're ready to start patching
+		OnPatchReady.Broadcast(bSuccess);
+	};
+
+	// Start the manifest update process, passing the BuildID and Lambda callback
+	Downloader->UpdateBuild(DeploymentName, ContentBuildId, ManifestCompleteCallback);
 
 }
 
